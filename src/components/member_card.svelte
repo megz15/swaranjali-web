50bdde33a978cc1883ef07f4c24615da6a6ce276
megz15 <meghraj.g16@gmail.com>
2023-11-15 01:58:00 +0530

    feat: move to flowbite card

--- src/components/member_card.svelte
+++ src/components/member_card.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+
+    import { Card, Avatar, Badge } from 'flowbite-svelte';
+
     export let pfp_url: string;
     export let names: string[];
     export let position: string;
@@ -12,12 +15,6 @@
         pos: string;
     };
 
-    let tagColours: memberTags = {
-        cat: "F4D8AA",
-        bat: "A4D8AA",
-        pos: "F4D8AA",
-    };
-
     function spoofName() {
         // displayedName = names[Math.floor(Math.random() * names.length)];
         displayedNameIndex = (displayedNameIndex + 1) % names.length;
@@ -25,59 +22,16 @@
 </script>
 
 <main>
-    <div class="card">
-        <img class="pfp" alt="member" src={pfp_url} />
-        <h2 on:click={spoofName}>{names[displayedNameIndex]}</h2>
-        <h3>{position}</h3>
-        <div class="tags">
-            {#each Object.keys(tags) as k}
-                <span style="background-color: #{tagColours[k]}">
-                    {tags[k]}
-                </span>
-            {/each}
+    <Card padding="sm" class="w-[350px] bg-[#12121fcc] rounded-3xl backdrop-blur-sm border-0">
+        <div class="flex flex-col items-center">
+            <Avatar src={pfp_url} class="w-44 h-44" />
+            <h5 on:click={spoofName} class="mt-8 mb-1 text-2xl font-medium text-white">{names[displayedNameIndex]}</h5>
+            <span class="text-xl text-yellow-300">{position}</span>
+            <div class="flex mt-8 space-x-3">
+                <Badge large color="yellow" class="text-lg px-3">{tags.cat}</Badge>
+                <Badge large color="green" class="text-lg px-3">{tags.bat}</Badge>
+                <Badge large color="yellow" class="text-lg px-3">{tags.pos}</Badge>
+            </div>
         </div>
-    </div>
+    </Card>
 </main>
-
-<style>
-    .card {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 350px;
-        height: 400px;
-        background-color: rgb(18, 18, 31, 0.8);
-        backdrop-filter: blur(6px);
-        color: #f3e9de;
-        border-radius: 28px;
-        box-shadow: 5px 5px 8px rgb(18, 18, 31, 0.8);
-    }
-
-    .pfp {
-        width: 175px;
-        border-radius: 50%;
-    }
-
-    h2 {
-        margin: 24px 0 0;
-        font-size: 28px;
-        cursor: pointer;
-    }
-    h3 {
-        margin: 0 0 42px;
-        font-size: 20px;
-        color: #ffde59;
-    }
-
-    .tags {
-        display: flex;
-        gap: 10px;
-        color: black;
-    }
-
-    .tags span {
-        padding: 8px 24px 8px 24px;
-        border-radius: 8px;
-    }
-</style>
