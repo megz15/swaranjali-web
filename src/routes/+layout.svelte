a3f4f81f1141e639f92a554c670c05c0b416e2d5
megz15 <meghraj.g16@gmail.com>
2023-11-15 02:22:31 +0530

    refactor+feat: rudimentary member page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,20 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script lang="ts">
+    import "../app.postcss";
+    import { Navbar, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+</script>
+
+<Navbar let:NavContainer class="m-0 p-0">
+    <NavContainer class="bg-[#1d2230b9] rounded-lg fixed z-10 top-2 justify-around backdrop-blur">
+    <NavHamburger />
+    <NavUl>
+        <NavLi class="text-black sm:text-white" href="/">Home</NavLi>
+        <NavLi class="text-black sm:text-white" href="/members">Members</NavLi>
+        <NavLi class="text-black sm:text-white" href="/naadgen">NaadGen</NavLi>
+        <NavLi class="text-black sm:text-white" href="/drive">Drive</NavLi>
+        <NavLi class="text-black sm:text-white" href="/social">Social</NavLi>
+        <NavLi class="text-black sm:text-white" href="/recordings">Recordings</NavLi>
+    </NavUl>
+    </NavContainer>
+</Navbar>
+
+<slot />
---
+++ src/routes/members/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import MemberCard from "../../components/member_card.svelte";
+    import memberData from "$lib/data/senate-members.json";
+</script>
+
+<main>
+    <section class="flex flex-wrap justify-center gap-5 pt-20 pb-8">
+        {#each [...memberData].reverse() as member, _}
+            <MemberCard
+                pfp_url={member.pfp_url}
+                position={member.position}
+                tags={member.tags}
+                names={member.names}
+            />
+        {/each}
+    </section>
+</main>
