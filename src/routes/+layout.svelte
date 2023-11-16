1870e143f444a58ed615b93131efee590eb23af7
megz15 <meghraj.g16@gmail.com>
2023-11-17 00:58:42 +0530

    fix: navbar min width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <Navbar let:NavContainer class="m-0 p-0">
-    <NavContainer class="bg-[#1d2230b9] rounded-lg fixed z-10 top-2 justify-around backdrop-blur">
-    <NavHamburger />
+    <NavContainer class="bg-[#1d2230b9] right-1/2 translate-x-1/2 rounded-lg fixed z-50 top-5 justify-around backdrop-blur w-min">
+    <!-- <NavHamburger /> -->
     <NavUl>
         <NavLi class="text-black sm:text-white" href="/">Home</NavLi>
         <NavLi class="text-black sm:text-white" href="/members">Members</NavLi>
